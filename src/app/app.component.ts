7cda560363fee3dac336c2fec47761efe7652f1c
Emils <135007928+emilsViksnins@users.noreply.github.com>
2024-01-13 09:32:13 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Observable, catchError, map, of, startWith } from 'rxjs';
+import { ApiResponse } from './interface/api-response';
+import { HttpClientModule, HttpErrorResponse } from '@angular/common/http';
+import { Page } from './interface/page';
+import { UserService } from './service/user.service';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
+  
 })
-export class AppComponent {
-  title = 'studentsapp';
+export class AppComponent implements OnInit {
+  usersState$: Observable<{
+    appState: string;
+    appData?: ApiResponse<Page>;
+    error?: HttpErrorResponse;
+  }>;
+
+  constructor(private userSevice: UserService) {}
+
+  ngOnInit(): void {
+    this.usersState$ = this.userSevice.users$().pipe(
+      map((response: ApiResponse<Page>) => {
+        console.log(response);
+        return { appState: 'APP_LOADED', appData: response };
+      }),
+      startWith({ appState: 'APP_LOADING' }),
+      catchError((error: HttpErrorResponse) =>
+        of({ appState: 'APP_ERROR', error })
+      )
+    );
+  }
 }
