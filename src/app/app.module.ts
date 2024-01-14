b49469acc33952acfa03b2a938850414ec53b4cd
Emils <135007928+emilsViksnins@users.noreply.github.com>
2024-01-14 13:54:45 +0200

    corrected code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { HttpClientModule } from "@angular/common/http";
-import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
+import { FormsModule } from "@angular/forms";
 
 
 @NgModule({
     declarations: [
-       
+        
         
     ],
     imports: [
@@ -16,6 +16,7 @@ import { AppComponent } from "./app.component";
         AppRoutingModule,
         HttpClientModule,
         FormsModule
+       
     ],
     providers: [],
     bootstrap: []
